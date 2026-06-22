c8e2ac5fca29f03c3bd1ae2f277aa83e7f46af17
Slimane ZZZ <285789629+SlimaneZZZ@users.noreply.github.com>
2026-06-22 10:33:06 +0200

    Premières mises en place architecture du code

---
+++ Urban Canvas/Views/Screens/DetailOeuvre.swift
@@ -0,0 +1,18 @@
+//
+//  DetailOeuvre.swift
+//  Urban Canvas
+//
+//  Created by Petit dragon on 22/06/2026.
+//
+
+import SwiftUI
+
+struct DetailOeuvre: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailOeuvre()
+}
---
+++ Urban Canvas/Views/Screens/ListeOeuvres.swift
@@ -0,0 +1,18 @@
+//
+//  ListeOeuvres.swift
+//  Urban Canvas
+//
+//  Created by Petit dragon on 22/06/2026.
+//
+
+import SwiftUI
+
+struct ListeOeuvres: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListeOeuvres()
+}
