b98aba68a90b1b5229965bbf26b161d9b8089259
Slimane ZZZ <285789629+SlimaneZZZ@users.noreply.github.com>
2026-06-22 09:45:07 +0200

    Initial Commit

---
+++ Urban Canvas/Urban_CanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Urban_CanvasApp.swift
+//  Urban Canvas
+//
+//  Created by Petit dragon on 22/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Urban_CanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
